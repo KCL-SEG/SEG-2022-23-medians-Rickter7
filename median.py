ca629043a97fbb29c1d52e751bfe01a1fe187fb1
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-09-26 13:08:34 +0100

    Task completed. Median found for various lists

--- median.py
+++ median.py
@@ -5,8 +5,18 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        median = 0
+        midIndex = int(len(numbers) / 2)
+        numbers.sort()
+        if len(numbers) % 2 == 0:
+            median = (numbers[midIndex] + numbers[midIndex-1]) / 2
+            print(median)
+        else:
+            median = numbers[midIndex]
+            print(median)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
 print(numbers)
+
